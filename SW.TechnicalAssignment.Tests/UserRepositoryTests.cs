400e7b32470b8c691df0866d55d3811cc9fa0c90
Neil Cross <ncross@macquarietelecom.com>
2019-09-04 00:12:54 +1000

    Finish writing tests

--- SW.TechnicalAssignment.Tests/UserRepositoryTests.cs
+++ SW.TechnicalAssignment.Tests/UserRepositoryTests.cs
@@ -5,11 +5,12 @@ namespace SW.TechnicalAssignment.Tests
     using Moq;
     using Shouldly;
     using System.Collections.Generic;
+    using System.Linq;
     using Xunit;
 
+    using DataAccess;
     using Models;
     using Interfaces;
-    using System.Linq;
 
     public class UserRepositoryTests
     {
@@ -23,10 +24,10 @@ namespace SW.TechnicalAssignment.Tests
         }
 
         [Fact]
-        public void CanFindUserWithId()
+        public void GetByIdCanFindUserWithId()
         {
             // Arrange
-            var testData = this.fixture.CreateMany<UserModel>().ToList();
+            var testData = this.fixture.Create<List<UserModel>>();
             this.mockDataSource.Setup(l => l.GetUsers()).Returns(testData);
 
             var expected = testData.First();
@@ -41,10 +42,10 @@ namespace SW.TechnicalAssignment.Tests
         }
 
         [Fact]
-        public void ReturnsullWithInvalidId()
+        public void GetByIdReturnsullWithInvalidId()
         {
             // Arrange
-            var testData = fixture.Create<List<UserModel>>();
+            var testData = this.fixture.Create<List<UserModel>>();
             this.mockDataSource.Setup(l => l.GetUsers()).Returns(testData);
 
             var sut = fixture.Create<UserRepository>();
@@ -54,6 +55,87 @@ namespace SW.TechnicalAssignment.Tests
 
             //Assert
             result.ShouldBeNull();
+        }
+
+        [Theory]
+        [InlineData(34, 1)]
+        [InlineData(32, 2)]
+        [InlineData(26, 0)]
+        public void GetUsersByAgeReturnsUsersWithCorrectAge(int testAge, int expectedUsers)
+        {
+            // Arrange
+            var testData = new List<UserModel>()
+            {
+                new UserModel { Age = 32 },
+                new UserModel { Age = 34 },
+                new UserModel { Age = 35 },
+                new UserModel { Age = 32 },
+                new UserModel { Age = 36 },
+            };
+            this.mockDataSource.Setup(l => l.GetUsers()).Returns(testData);
+
+            var sut = fixture.Create<UserRepository>();
+
+            // Act
+            var result = sut.GetUsersByAge(testAge);
+
+            // Assert
+            result.ShouldAllBe(u => u.Age == testAge);
+            result.Count().ShouldBe(expectedUsers);
+        }
+
+
+
+        [Fact]
+        public void GenerateAgeSummariesAccumulatesGenderCounts()
+        {
+            // Arrange
+            var testData = new List<UserModel>()
+            {
+                new UserModel { Age = 33, Gender = Gender.Female },
+                new UserModel { Age = 35, Gender = Gender.Male  },
+                new UserModel { Age = 33, Gender = Gender.Female },
+                new UserModel { Age = 34, Gender = Gender.Female},
+                new UserModel { Age = 32, Gender = Gender.Male },
+                new UserModel { Age = 34 },
+                new UserModel { Age = 33 },
+            };
+            this.mockDataSource.Setup(l => l.GetUsers()).Returns(testData);
+
+            var sut = fixture.Create<UserRepository>();
+
+            // Act
+            var result = sut.GenerateAgeSummaries();
+
+            // Assert
+            var item = result.First(s => s.Age == 33);
+            item.GenderCounts[Gender.Female].ShouldBe(2);
+            item.GenderCounts[Gender.Other].ShouldBe(1);
+            item.GenderCounts.ContainsKey(Gender.Male).ShouldBe(false);
+        }
+
+        [Fact]
+        public void GenerateAgeSummariesSortsByAge()
+        {
+            // Arrange
+            var testData = new List<UserModel>()
+            {
+                new UserModel { Age = 36 },
+                new UserModel { Age = 34 },
+                new UserModel { Age = 31 },
+                new UserModel { Age = 36 },
+                new UserModel { Age = 35 },
+            };
+            this.mockDataSource.Setup(l => l.GetUsers()).Returns(testData);
+
+            var sut = fixture.Create<UserRepository>();
+
+            // Act
+            var result = sut.GenerateAgeSummaries();
+
+            // Assert
+            var item = result.First();
+            item.Age.ShouldBe(31);
         }
     }
 }
